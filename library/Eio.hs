855c0289490e4039f1e1c73cc625c66c8b9d606e
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-01-14 12:49:51 +0300

    Implement around IO

--- library/Eio.hs
+++ library/Eio.hs
@@ -1,5 +1,20 @@
 module Eio
+(
+  Eio,
+)
 where
 
 import Eio.Prelude
 
+
+newtype Eio err res =
+  Eio (IO res)
+  deriving (Functor, Applicative, Monad, MonadFail)
+
+instance MonadIO (Eio SomeException) where
+  liftIO = Eio
+
+{-| Low-level helper. -}
+mapIO :: (IO a -> IO b) -> Eio oldErr a -> Eio newErr b
+mapIO mapper =
+  coerce . mapper . coerce
