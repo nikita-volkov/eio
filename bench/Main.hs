a67468749a72dbf0dbba07c025735d6eeba6ea04
Nikita Volkov <nikita.y.volkov@mail.ru>
2021-01-21 14:41:55 +0300

    Add benchmark

---
+++ bench/Main.hs
@@ -0,0 +1,63 @@
+module Main where
+
+import Prelude
+import Gauge.Main
+import qualified EIO
+
+
+data CustomException =
+  CustomException
+  deriving (Show, Typeable, Generic)
+
+instance NFData CustomException
+
+instance Exception CustomException
+
+main =
+  do
+    ref <- newIORef @Int 0
+    defaultMain [
+      bgroup "Handling failure" [
+        bench "EIO" $ nfIO $ EIO.runEIO $ do
+          EIO.try
+            (do
+              liftIO (writeIORef ref 0)
+              EIO.throw CustomException
+              liftIO (writeIORef ref 1)
+              )
+        ,
+        bench "ExceptT" $ nfIO $ runExceptT $ do
+          catchError
+            (do
+              liftIO (writeIORef ref 0)
+              throwError CustomException
+              liftIO (writeIORef ref 1)
+              return (Right ())
+              )
+            (return . Left)
+        ,
+        bench "IO" $ nfIO $ do
+          try @CustomException (do
+            liftIO (writeIORef ref 0)
+            throwIO CustomException
+            liftIO (writeIORef ref 1)
+            )
+        ]
+      ,
+      bgroup "Success" [
+        bench "EIO" $ nfIO $ EIO.runEIO $ do
+          liftIO (writeIORef ref 0)
+          a <- liftIO (readIORef ref)
+          liftIO (writeIORef ref $! succ a)
+        ,
+        bench "ExceptT" $ nfIO $ runExceptT @() $ do
+          liftIO (writeIORef ref 0)
+          a <- liftIO (readIORef ref)
+          liftIO (writeIORef ref $! succ a)
+        ,
+        bench "IO" $ nfIO $ do
+          writeIORef ref 0
+          a <- readIORef ref
+          writeIORef ref $! succ a
+        ]
+      ]
