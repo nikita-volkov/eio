789bb8f135b810834020241c80aaa7d297118f17
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-12-15 12:35:09 +0300

    Update the benches

--- bench/Main.hs
+++ bench/Main.hs
@@ -17,46 +17,48 @@ main =
   do
     ref <- newIORef @Int 0
     defaultMain [
+      bgroup "Success" [
+        bench "EIO" $ nfIO $ EIO.runEIO $ do
+          replicateM_ 10 $ liftIO (writeIORef ref 0)
+          a <- liftIO (readIORef ref)
+          liftIO (writeIORef ref $! succ a)
+        ,
+        bench "IO" $ nfIO $ do
+          replicateM_ 10 $ liftIO (writeIORef ref 0)
+          a <- readIORef ref
+          writeIORef ref $! succ a
+        ]
+        ,
+        bench "ExceptT" $ nfIO $ runExceptT @() $ do
+          replicateM_ 10 $ liftIO (writeIORef ref 0)
+          a <- liftIO (readIORef ref)
+          liftIO (writeIORef ref $! succ a)
+      ,
       bgroup "Handling failure" [
         bench "EIO" $ nfIO $ EIO.runEIO $ do
           EIO.try
             (do
-              liftIO (writeIORef ref 0)
+              replicateM_ 10 $ liftIO (writeIORef ref 0)
               EIO.throw CustomException
               liftIO (writeIORef ref 1)
               )
         ,
         bench "IO" $ nfIO $ do
           try @CustomException (do
-            liftIO (writeIORef ref 0)
+            replicateM_ 10 $ liftIO (writeIORef ref 0)
             throwIO CustomException
             liftIO (writeIORef ref 1)
             )
         ],
+        -- This one proves to be much faster than both EIO and IO,
+        -- because it avoids the instructions in 'catch'.
         bench "ExceptT" $ nfIO $ runExceptT $ do
           catchError
             (do
-              liftIO (writeIORef ref 0)
+              replicateM_ 10 $ liftIO (writeIORef ref 0)
               throwError CustomException
               liftIO (writeIORef ref 1)
               return (Right ())
               )
             (return . Left)
-      ,
-      bgroup "Success" [
-        bench "EIO" $ nfIO $ EIO.runEIO $ do
-          liftIO (writeIORef ref 0)
-          a <- liftIO (readIORef ref)
-          liftIO (writeIORef ref $! succ a)
-        ,
-        bench "IO" $ nfIO $ do
-          writeIORef ref 0
-          a <- readIORef ref
-          writeIORef ref $! succ a
-        ]
-        ,
-        bench "ExceptT" $ nfIO $ runExceptT @() $ do
-          liftIO (writeIORef ref 0)
-          a <- liftIO (readIORef ref)
-          liftIO (writeIORef ref $! succ a)
       ]
